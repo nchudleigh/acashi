379ba3444a80c8348899ba730b189fe2d307c405
Neil Chudleigh <nchudleigh@gmail.com>
2019-03-23 14:52:06 -0400

    Handler init check for go method

--- src/handler/__init__.py
+++ src/handler/__init__.py
@@ -1,2 +1,5 @@
 class Handler:
-    pass
+    def __init__(self):
+        go_func = getattr(self, "go")
+        if not go_func and callable(go_func):
+            raise NotImplementedError("Handler's must define a go function")
