1694da0497fc587ee1bffe03e64945a8aef44ff2
Neil Chudleigh <nchudleigh@gmail.com>
2019-03-25 09:13:23 -0400

    Password model, get by user_key

--- src/domain/model/password.py
+++ src/domain/model/password.py
@@ -7,3 +7,6 @@ PasswordDTO = namedtuple("Password", ["key", "hashed", "salt", "user_key"])
 
 class Password(Model):
     DTO = PasswordDTO
+
+    def get_by_user_key(self, user_key):
+        self.get("user_key", user_key)
