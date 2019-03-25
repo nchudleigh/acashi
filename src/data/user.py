9ffffab8309f0a3e0aa9770c482d047fe4e976dd
Neil Chudleigh <nchudleigh@gmail.com>
2019-03-25 09:07:21 -0400

    get_by_email to UserRepo

--- src/data/user.py
+++ src/data/user.py
@@ -5,3 +5,6 @@ from src.domain.model.user import UserDTO
 class UserRepo(MemRepo):
     DTO = UserDTO
     table_name = "user"
+
+    def get_by_email(self, email):
+        return self.get_by_attr("email", email)
