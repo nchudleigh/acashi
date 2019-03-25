43bd15423aaeb3d59e46ba8b177b3d5677f8025e
Neil Chudleigh <nchudleigh@gmail.com>
2019-03-25 09:08:57 -0400

    Token authentication handler integrated into rest layer

--- src/rest/base.py
+++ src/rest/base.py
@@ -4,26 +4,24 @@ from flask.views import View
 from flask import Response, request
 
 from src.rest.exceptions import AuthorizationError
+from src.handler.auth import DecodeTokenHandler
 
 
 class BaseView(View):
     def dispatch_request(self, **kwargs):
         try:
+            self._authenticate()
             endpoint = getattr(self, request.method.lower())
             response = endpoint(request, **kwargs)
             response = self.to_json_response(response)
         except (TypeError, ValueError):
             response = self.respond(400, "Bad Request")
-            raise
         except AuthorizationError:
             response = self.respond(403, "Unauthorized")
-            raise
         except LookupError:
             response = self.respond(404, "Not Found")
-            raise
         except Exception:
             response = self.respond(500, "Internal Server Error")
-            raise
 
         return response
 
@@ -33,3 +31,37 @@ class BaseView(View):
 
     def respond(self, status, body, mimetype="text/html"):
         return Response(status=status, response=body, mimetype=mimetype)
+
+    def _authenticate(self):
+        self._authenticate_token()
+
+    def _authenticate_token(self):
+        try:
+            encoded_token = self._get_token_from_header()
+            decode_token = DecodeTokenHandler()
+            response = decode_token.run(encoded_token)
+        except Exception as e:
+            print(e)
+            raise AuthorizationError("Invalid Token")
+
+    def _get_token_from_header(self):
+        authorization_header = request.headers.get("Authorization")
+        if authorization_header is None:
+            raise AuthorizationError("No token given")
+        encoded_token = authorization_header.split(" ")[1]
+        return encoded_token
+
+    # TODO: bring key based authentication into the mix
+    # def _authenticate_keys(self):
+    #     try:
+    #         public_key, private_key = self._get_keys_from_request()
+    #         check_keys = CheckKeysHandler()
+    #         response = check_keys.run(public_key, private_key)
+    #     except Exception as e:
+    #         return Response(success=False)
+    #
+    # def _get_keys_from_request(self):
+    #     credentials = request.authentication
+    #     public_key = credentials.username
+    #     private_key = credentials.username
+    #     return public_key, private_key
