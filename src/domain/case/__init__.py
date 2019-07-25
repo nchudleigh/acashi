cc4b153fb5671032453123008d3b4303f9d25487
Neil Chudleigh <nchudleigh@gmail.com>
2019-07-24 21:53:22 -0400

    Case Updates

--- src/domain/case/__init__.py
+++ src/domain/case/__init__.py
@@ -1,10 +1,11 @@
 class Case:
-    def __init__(self, Repo):
-        if Repo:
-            self.repo = Repo()
+    def __init__(self, *kw):
+        for repo_name, RepoClass in kw.items():
+            if getattr(RepoClass, is_repo, False):
+                setattr(self, repo_name, RepoClass)
 
     def run(self, *args, **kwargs):
         return self.go(*args, **kwargs)
 
     def go(self):
-        raise NotImplementedError()
+        raise NotImplementedError("Cases must implement a go function")
